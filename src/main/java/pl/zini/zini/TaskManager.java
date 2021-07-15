0d091e053ff618802e9340223b5f90c68a4cbd43
Pawel Ziniewicz <pziniewicz@Pawels-MacBook-Air.local>
2021-07-16 01:00:57 +0200

    First version of app completed.

--- src/main/java/pl/zini/zini/TaskManager.java
+++ src/main/java/pl/zini/zini/TaskManager.java
@@ -1,4 +1,103 @@
 package pl.zini.zini;
 
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class TaskManager {
+
+    static LinkedList<String> taskList = new LinkedList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        loadFile();
+        while (true) {
+            System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + "Please select an option:");
+            System.out.println(ConsoleColors.GREEN_BRIGHT + "add");
+            System.out.println(ConsoleColors.RED_BRIGHT + "remove");
+            System.out.println(ConsoleColors.BLUE_BRIGHT + "list");
+            System.out.println(ConsoleColors.RESET + "exit");
+            System.out.print("$ ");
+            String selection = scanner.nextLine();
+            if (selection.equals("exit")) {
+                writeFile(taskList);
+                break;
+            }
+            switch (selection) {
+                case "add":
+                    add();
+                    break;
+                case "remove":
+                    list();
+                    remove();
+                    break;
+                case "list":
+                    list();
+                    scanner.nextLine();
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+            System.out.flush();
+        }
+
+
+    }
+
+    public static LinkedList<String> loadFile() {
+        try {
+            Scanner fileScanner = new Scanner(new File("tasks.csv"));
+            while (fileScanner.hasNextLine()) {
+                taskList.add(fileScanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(taskList);
+        return taskList;
+    }
+
+    public static void writeFile(LinkedList<String> taskList) {
+        try {
+            FileWriter fileWriter = new FileWriter("tasks.csv");
+            for (String s : taskList) {
+                fileWriter.append(s).append("\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void add() {
+        System.out.println("Please add task description");
+        String taskDescription = scanner.nextLine();
+        System.out.println("Please add task due date");
+        String taskDueDate = scanner.nextLine();
+        System.out.println("Is your task important: true/false");
+        String taskImportance = scanner.nextLine();
+        taskList.add(taskDescription + ", " + taskDueDate + ", " + taskImportance);
+    }
+
+    public static void remove() {
+        System.out.println("Please select number to remove");
+        try {
+            int positionToRemove = scanner.nextInt();
+            System.out.println("Value was succesfully removed: " + taskList.get(positionToRemove-1));
+            taskList.remove(positionToRemove-1);
+        } catch (Exception e) {
+            System.out.println("Please select a correct option.");
+        }
+        scanner.nextLine();
+
+    }
+
+    public static void list() {
+        System.out.println("list");
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println((i+1)+ ". " + taskList.get(i));
+        }
+    }
+
 }
